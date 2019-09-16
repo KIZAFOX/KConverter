b5508fbdcdd10c33fe04d0c0872da3722fbb5de8
Victor <victor.pernak@hotmail.fr>
2019-09-16 22:17:27 +0200

    MySQL tables modify.

--- src/fr/kizafox/binaryconverter/Binary.java
+++ src/fr/kizafox/binaryconverter/Binary.java
@@ -4,7 +4,7 @@ package fr.kizafox.binaryconverter;
  * --
  * Le projet a été crée par KIZAFOX. (workspace)
  * Dans le package fr.kizafox.binaryconverter.
- * Le lundi 16 septembre 2019 à 21h58.
+ * Le lundi 16 septembre 2019 à 22h06.
  * --
  * Copyright © 2019 by KIZAFOX.
  * All rights reserved. This java project or any portion thereof
@@ -14,9 +14,24 @@ package fr.kizafox.binaryconverter;
  * --
  */
 
-public class Main {
+public class Binary {
 
-    public static void main(String... args){
-        System.out.println("Decimal of 11111111 is: " + Binary.getDecimal(11111111));
+
+    static int getDecimal(int binary){
+        int decimal = 0;
+        int n = 0;
+
+        while (true){
+            if(binary == 0){
+                break;
+            }else{
+                int temp = binary%10;
+                decimal += temp*Math.pow(2, n);
+                binary = binary/10;
+                n++;
+            }
+        }
+        return decimal;
     }
+
 }
