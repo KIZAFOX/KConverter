d032f796f5ffc5da68d1818caa204aaaa8e88b9f
KIZAFOX <victor.pernak@hotmail.fr>
2021-02-15 20:15:06 +0100

    [1.5] KConverter initial

--- src/fr/kizafox/kconverter/KConverter.java
+++ src/fr/kizafox/kconverter/KConverter.java
@@ -3,7 +3,6 @@ package fr.kizafox.kconverter;
 /**
  * --
  * Project create by KIZAFOX on 09/16/2019 in {@link KConverter} (Modified on 02/15/2021)
- * Converter class in {@link Converter}
  * --
  * Copyright © 2019 by KIZAFOX.
  * All rights reserved. This java project or any portion thereof
@@ -14,10 +13,76 @@ package fr.kizafox.kconverter;
  */
 public final class KConverter {
 
-    public static void main(String... args){
-        System.out.println("Decimal of 11111111 is: " + Converter.binaryToDecimal(11111111));
-        System.out.println("Binary of 255 is: " + Converter.decimalToBinary(255));
-        System.out.println("Decimal of \"A\" is: " + Converter.hexadecimalToDecimal("A"));
-        System.out.println("Hexadecimal of 10 is: " + Converter.decimalToHexadecimal(10));
+    /**
+     * This method converts a number to a decimal
+     * @param binary a number to be added to convert
+     * @return Integer
+     */
+    protected static int binaryToDecimal(int binary) {
+        int decimal = 0;
+        int n = 0;
+
+        while (true) {
+            if (binary == 0) {
+                break;
+            } else {
+                int temp = binary % 10;
+                decimal += temp * Math.pow(2, n);
+                binary = binary / 10;
+                n++;
+            }
+        }
+        return decimal;
+    }
+
+    /**
+     * This method converts a decimal to a number
+     * @param decimal a number to be added to convert
+     * @return Integer
+     */
+    protected static int decimalToBinary(int decimal) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int base = 2;
+        while (decimal != 0) {
+            int reminder = decimal % base;
+            stringBuilder.append(reminder);
+            decimal = decimal / base;
+        }
+        return Integer.parseInt(stringBuilder.reverse().toString());
+    }
+
+    /**
+     * This method converts a hexadecimal to a decimal
+     * @param hexadecimal a text to be added to convert
+     * @return String
+     */
+    protected static int hexadecimalToDecimal(String hexadecimal) {
+        String digits = "0123456789ABCDEF";
+        hexadecimal = hexadecimal.toUpperCase();
+        int val = 0;
+        for (int i = 0; i < hexadecimal.length(); i++) {
+            char c = hexadecimal.charAt(i);
+            int d = digits.indexOf(c);
+            val = 16 * val + d;
+        }
+        return val;
+    }
+
+    /**
+     * This method converts a decimal to a hexadecimal
+     * @param decimal a number to be added to convert
+     * @return Integer
+     */
+    protected static String decimalToHexadecimal(int decimal) {
+        int rem;
+        StringBuilder hexadecimal = new StringBuilder();
+        char[] hexchars = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+
+        while (decimal > 0) {
+            rem = decimal % 16;
+            hexadecimal.insert(0, hexchars[rem]);
+            decimal = decimal / 16;
+        }
+        return hexadecimal.toString();
     }
 }
